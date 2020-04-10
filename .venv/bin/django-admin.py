68bf1db5e8160270a7b4b9a979f8b2e7386f332f
Rafael Douglas <rafaeldouglas@abaweb.org>
2020-04-10 14:30:54 -0300

    remove env files

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rafael/Desktop/Cursos/django-vuejs-base/django-vuejs-project-base/.venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
